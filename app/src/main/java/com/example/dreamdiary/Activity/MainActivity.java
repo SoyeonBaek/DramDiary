6c1c01036793bac79d69120d20cd6f2c9467447d
SoyeonBaek <soyeon.baek28@gmail.com>
2021-12-05 14:12:28 +0900

    init

---
+++ app/src/main/java/com/example/dreamdiary/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dreamdiary.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dreamdiary.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/dreamdiary/Data/Diary.java
@@ -0,0 +1,4 @@
+package com.example.dreamdiary.Data;
+
+public class Diary {
+}

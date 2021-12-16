6e9aeb05198bb72b2136343f7ffdbb22da2aab62
siontama <siontama@naver.com>
2021-12-17 03:30:51 +0900

    statistic

--- app/src/main/java/com/example/dreamdiary/Activity/StatisticActivity.java
+++ app/src/main/java/com/example/dreamdiary/Activity/StatisticActivity.java
@@ -11,12 +11,17 @@ import com.example.dreamdiary.R;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class StatisticActivity extends AppCompatActivity {
 
@@ -34,11 +39,24 @@ public class StatisticActivity extends AppCompatActivity {
         SharedPreferences sp = getSharedPreferences("shared", MODE_PRIVATE);
 
         Map<String, ?> allEntries = sp.getAll();
+        Comparator<Integer> comparator = (s1, s2)->s2.compareTo(s1);
+        Map<Integer, Integer> cnt = new TreeMap<Integer, Integer>(comparator);
 
         for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
             Diary diary = gson.fromJson(entry.getValue().toString(), Diary.class);
-            diary.getEmoji();
-            System.out.println(diary.getEmoji());
+            int emoji = diary.getEmoji();
+            if(cnt.containsKey(emoji)) {
+                cnt.put(emoji, cnt.get(emoji) + 1);
+            } else {
+                cnt.put(emoji, 1);
+            }
+        }
+
+        int index = 0;
+        for (Map.Entry<Integer, Integer> entry : cnt.entrySet()) {
+            entries.add(new BarEntry(entry.getKey(), entry.getValue()));
+            index++;
+            if (index == 5) break;
         }
 
         BarDataSet barDataSet = new BarDataSet(entries, "emoji");
